b8ff846df8c8ddb358dc5cf38a0dcd4ab4d60393
Louis Kinley <louis.kinley@gmail.com>
2014-10-27 23:49:36 -0700

    corrected

--- public/javascripts/app/angular_app.js
+++ public/javascripts/app/angular_app.js
@@ -6,5 +6,5 @@ APP.config(function($routeProvider) {
 		.when('/bio', {templateUrl: 'partials/bio.html', controller: 'bioController'})
 		.when('/projects', {templateUrl: 'partials/carousel.html', controller: 'projectController'})
 		.when('/resume', {templateUrl: 'partials/resume.html', controller: 'resumeController'})
-		.otherwise({templateUrl: 'partials/front.html', controller: 'mainController'})
+		.otherwise({templateUrl: 'partials/front.html', controller: 'mainController'});
 });
