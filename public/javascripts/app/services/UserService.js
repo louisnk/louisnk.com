a25d2437417c45316b858d30da8b330ebcd5343f
loukinle <louis.kinley@gmail.com>
2015-03-28 19:31:23 -0700

    Does lots of user-y things

--- public/javascripts/app/services/UserService.js
+++ public/javascripts/app/services/UserService.js
@@ -1,37 +1,110 @@
-LnkAPP.factory("UserService", ["$rootScope", "DataService", "Constants", 
-	function($rootScope, DataService, Constants) {
+LnkAPP.factory("UserService", ["$rootScope", "$cookies", "DataService", "Constants", 
+	function($rootScope, $cookies, DataService, Constants) {
 
 		function User(info) {
-			for (var prop in info) {
-				this[prop] = info[prop];
+			firstName: info.firstName || "",
+			github: info.github || "",
+			twitter: info.twitter || "",
+			access: false,
+			logOut: function() {
+				$rootScope.broadcast(Constants.REQUESTS.USER.LOG_OUT, true);
 			}
 		};
-
 
 		var init = function() {
 			var user = getUser();
-			if (user.firstName) {
-				$rootScope.broadcast(Constants.EVENT.USER.FOUND, user);
+			listenForUserChanges();
+
+			if (user && user.firstName) {
+				return $rootScope.broadcast(Constants.EVENT.USER.FOUND, user);
 			} else {
-				user = new User({
-					firstName: "",
-					github: "",
-					twitter: "",
-					access: false
-				});
-			}
+				return $rootScope.broadcast(Constants.EVENT.USER.NEW, new User());
+			};
+
 		};
 
-		var getUserFromLocalstorage = function() {
+		var listenForUserChanges = function() {
+			$rootScope.on(Constants.EVENT.USER.CHANGED, updateUser);
+		};
+
+		var checkUser = function() {
+			return user.firstName &&
+						 user.github &&
+						 user.twitter &&
+						 user.hasOwnProperty(access) &&
+						 user.hasOwnProperty(logOut);
+		};
+
+		var getUser = function() {
+			var user = {};
 			try {
 				if (window.localStorage) {
-					var user = window.localStorage.getItem("LnkUser");
-
+					user = window.localStorage.getItem("LnkUser");
 				} else {
+					user = $cookies.getObject("LnkUser");
+				}
 
+				if (!user.firstName) {
+					return false;
+				} else {
+					return user;
 				}
 			} catch (e) {
-
+				console.log("browser doesn't have local storage? ", e);
+				return false;
 			}
 		};
+
+		var setUser = function(user) {
+			if (checkUser(user)) {
+				try {
+					
+					if (window.localStorage) {
+						window.localStorage.setItem("LnkUser", user);
+					} else {
+						$cookies.putObject("LnkUser", user);
+					}
+					
+					var savedUser = new Promise(function(resolve, reject) {	
+						var requestUrl = "/users";
+						
+						DataService.post(requestUrl, user).then(function(success) {
+							return resolve(true);
+							// console.log(success);
+						}).catch(function(err) {
+							return reject(err);
+							// console.log(err);
+						});
+					});
+
+					// return the result of getting the user,
+					// so that we can be sure it got set correctly
+					return savedUser && getUser("LnkUser");
+
+				} catch (e) {
+					console.log("browser doesn't have local storage? ", e);
+					return false;
+				}
+			} else {
+				console.log("Invalid user object passed to setUser");
+				return false;
+			}
+		};
+
+		var updateUser = function(user) {
+			if (checkUser(user)) { // it's a valid user object
+				if (setUser(user)) {
+					// re-use the same event, which leads to this method -
+					// this will trigger it to do nothing, but allow other
+					// systems to react to the change.
+					$rootScope.broadcast(Constants.EVENT.USER.CHANGED, true);
+				}
+			} else {
+				return false;
+			}
+		};
+
+		return {
+			init: init
+		}
 }]);
